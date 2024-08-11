a162c16f3061227be891abd71520a2619e9902ed
Kaio Rodrigo Guerreiro Gomes <guerreirokaio@gmail.com>
2024-08-11 18:56:33 -0400

    atividades 1 e 2

--- src/atividades/Atividade03.java
+++ src/atividades/Atividade03.java
@@ -1,4 +1,11 @@
+/*Crie um programa que preencha um array com 5 números inteiros
+fornecidos pelo usuário e calcule a soma desses números.
+ */
+
 package atividades;
 
 public class Atividade03 {
+    public static void main(String[] args) {
+
+    }
 }
